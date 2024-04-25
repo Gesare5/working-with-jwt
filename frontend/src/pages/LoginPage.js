9b7e132c930e755ac54dcf21a3ae374df344df45
Gesare5 <gesare.nyangweso@gmail.com>
2024-04-25 20:02:03 +0300

    Add: State to hold user input Add: Change handler for userinput

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginPage = () => {
+    const [loginInfo, setLoginInfo] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleOnChange = (evt)=>{
+        const { name, value } = evt.target;
+        setLoginInfo({
+            ...loginInfo,
+            [name]: value
+        });
+    };
     return (
         <div>
             <form>
-               <input type='text' name='username' placeholder='Enter username' /> 
-                <input type='password' name='password' placeholder='Enter Password' /> 
+               <input type='text' name='username' placeholder='Enter username' value={loginInfo.username} onChange={handleOnChange}/> 
+                <input type='password' name='password' placeholder='Enter Password' value={loginInfo.password} onChange={handleOnChange}/> 
                 <input type='submit'/>
             </form>
         </div>
