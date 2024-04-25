2de2f5d56b65f8c11eba2d00f061e695fadaecd7
Gesare5 <gesare.nyangweso@gmail.com>
2024-04-25 19:49:45 +0300

    Add: Router component to perform routing between pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      Learn React
+    <Router>
+      <Header/>
+      <Route Component={HomePage} path='/' exact/>
+      <Route Component={LoginPage} path='/login' />
+    </Router>
     </div>
   );
 }
