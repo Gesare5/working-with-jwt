ec145c789e321048608fa7d55ced7fff793974f1
Gesare5 <gesare.nyangweso@gmail.com>
2024-04-26 17:12:20 +0300

    Add: Authenticated state to check restrict access to private routes

--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import Header from "../components/Header";
 
 
 const PrivateRoute = (props) => {
     const { component: Component} = props;
+    const authenticated = false
 
     return (
         <div>
             <Header />
-            <Component />
+            {!authenticated ?
+                <Navigate to='/login' replace /> :
+                <Component />
+            }
         </div>
     )
 }
