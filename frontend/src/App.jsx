108857ae656fdf1cd9fd988f0e6e813f1f17c0ba
Gesare5 <gesare.nyangweso@gmail.com>
2024-04-26 17:00:54 +0300

    Fix: Move Header component out of Routes to resolve: "react__WEBPACK_IMPORTED_MODULE_0__.useContext(...) is null " error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from './components/Header';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-    <Header/>
     <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path='/' element={<PrivateRoute component={HomePage} />} />
+          <Route path='/login' element={<PrivateRoute component={LoginPage} />} /> 
         </Routes>
     </Router>
       
