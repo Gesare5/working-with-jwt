b8fe83a62ded4524e87e28b625faf5f6b399e2bc
Gesare5 <gesare.nyangweso@gmail.com>
2024-04-27 23:02:24 +0300

    Change: Wrap routes in AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import PrivateRoute from './utils/PrivateRoute';
@@ -7,13 +8,14 @@ import PrivateRoute from './utils/PrivateRoute';
 function App() {
   return (
     <div className="App">
-    <Router>
-        <Routes>
-          <Route path='/' element={<PrivateRoute component={HomePage} />} />
-          <Route path='/login' element={<PrivateRoute component={LoginPage} />} /> 
-        </Routes>
-    </Router>
-      
+      <AuthProvider>
+        <Router>
+            <Routes>
+              <Route path='/' element={<PrivateRoute component={HomePage} />} />
+              <Route path='/login' element={<LoginPage />} />  {/* this changed first */}
+            </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
