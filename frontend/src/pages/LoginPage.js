a7ecac9c8a476fb11e28ff7a80e5b6058e731d67
Gesare5 <gesare.nyangweso@gmail.com>
2024-07-03 13:58:23 +0300

    Change: Pass usercontext to LoginPage

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import AuthContext from '../context/AuthContext';
 
 const LoginPage = () => {
     const [loginInfo, setLoginInfo] = useState({
@@ -6,19 +7,29 @@ const LoginPage = () => {
         password: ''
     });
 
-    const handleOnChange = (evt)=>{
+    const handleOnChange = (evt) => {
         const { name, value } = evt.target;
         setLoginInfo({
             ...loginInfo,
             [name]: value
         });
     };
+
+    let { loginUser } = useContext(AuthContext);
+
+    const userSubmit = (e) => {
+        e.preventDefault();
+        if (loginInfo.username && loginInfo.password) {
+            loginUser(loginInfo.username, loginInfo.password)
+        }
+    }
+
     return (
         <div>
-            <form>
-               <input type='text' name='username' placeholder='Enter username' value={loginInfo.username} onChange={handleOnChange}/> 
-                <input type='password' name='password' placeholder='Enter Password' value={loginInfo.password} onChange={handleOnChange}/> 
-                <input type='submit'/>
+            <form onSubmit={userSubmit}>
+                <input type='text' name='username' placeholder='Enter username' value={loginInfo.username} onChange={handleOnChange} />
+                <input type='password' name='password' placeholder='Enter Password' value={loginInfo.password} onChange={handleOnChange} />
+                <input type='submit' />
             </form>
         </div>
     );
