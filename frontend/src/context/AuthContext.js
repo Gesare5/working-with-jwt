945c9e4366c723972c804072750c72a83f9795de
Gesare5 <gesare.nyangweso@gmail.com>
2024-07-03 13:57:03 +0300

    Change: Fetch tokens by sending username and password to token endpoint

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -6,9 +6,31 @@ const AuthContext = createContext();
 export default AuthContext;
 
 
-export const AuthProvider = ({children}) => {
-    return(
-        <AuthContext.Provider value={{'name': 'Ivy'}}>
+export const AuthProvider = ({ children }) => {
+
+    const [authTokens, setAuthTokens] = useState(null);
+    const [user, setUser] = useState(null);
+
+
+    const loginUser = async (name, pass) => {
+        // e.preventDefault();
+        console.log('form submitted')
+        let response = await fetch('http://127.0.0.1:8000/api/token/', {
+            method: 'POST',
+            Headers: {
+                'Content-Type': 'application/json'
+            },
+            body: { 'username': name, 'password': pass }
+        })
+        let data = await response.json()
+        console.log(response)
+        console.log('data', data)
+    }
+    let contextData = {
+        loginUser: loginUser
+    }
+    return (
+        <AuthContext.Provider value={contextData}>
             {children}
         </AuthContext.Provider>
     )
