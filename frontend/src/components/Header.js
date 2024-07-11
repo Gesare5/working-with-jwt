1bca3daa615554e9694f5da5625611797f8b1c7a
Gesare5 <gesare.nyangweso@gmail.com>
2024-07-11 19:10:38 +0300

    Add: Logout functionality to header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
 
 const Header = () => {
+    let { user, logoutUser } = useContext(AuthContext);
+    const navigate = useNavigate();
     return (
         <div>
             <Link to='/'>Home</Link>
             <span> | </span>
-            <Link to='/login'>Login</Link>
+            {user ? (<button onClick={() => logoutUser(navigate)}>Logout</button>) : (
+                <Link to='/login'>Login</Link>
+            )
+            }
+            {user && <p>Hello {user.username}</p>}
         </div>
     );
 };
