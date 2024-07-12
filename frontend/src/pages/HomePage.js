82e851882c9da52bcdeb40abae79eadcc635c4ee
Gesare5 <gesare.nyangweso@gmail.com>
2024-07-12 13:04:07 +0300

    Add:  Retrieve sample notes using getNotes function

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import AuthContext from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+    const navigate = useNavigate();
+    const [notes, setNotes] = useState([]);
+    const { authTokens, logoutUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        getNotes()
+    }, []);
+
+    let getNotes = async () => {
+        let response = await fetch('http://localhost:8000/notes/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + String(authTokens.access)
+            }
+        })
+        let data = response.json()
+        if (response.status === 200) {
+            setNotes(data);
+        }
+        else if (response.statusText === 'Unauthorized') {
+            logoutUser(navigate);
+        }
+    }
+
+
     return (
         <div>
             <p>You are logged in to the home page</p>
+            <ul>
+                {notes.map(note => (
+                    <li key={note.id}>{note.body}</li>
+                ))}
+            </ul>
         </div>
     );
 };
